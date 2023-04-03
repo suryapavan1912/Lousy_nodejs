aee8f4fb94b0da2541cf90adf0dae2ee329698a4
suryapavan1912 <pavansurya1912@gmail.com>
2023-04-03 18:45:57 +0530

    env added

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors'
-import { v4 as uuidv4 } from 'uuid';
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser';
+
+// import { v4 as uuidv4 } from 'uuid';
 import { filter , product } from './functions/product.js';
 import { Createuser , Updateuser } from './functions/user.js';
-import bodyParser from 'body-parser';
 import { Updatecart, Updatelist } from './functions/cart.js';
 import { Deleteitem, Deletewish } from './functions/deleteitem.js';
 
 const app = express();
 const Port = 9000;
+dotenv.config()
 
 const corsOptions ={ origin:'https://lousy-d1b70.web.app' , credentials:true , optionSuccessStatus:200 }
 app.use(cors(corsOptions));
+
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(bodyParser.json())
-app.get('/api/pavan', (req,res)=>res.json(uuidv4()))
+// app.get('/api/pavan', (req,res)=>res.json(uuidv4()))
 
 async function main() {
-  await mongoose.connect('mongodb+srv://pavan:pavan@cluster0.u9inhab.mongodb.net/?retryWrites=true&w=majority' );
+  await mongoose.connect(Process.env.MONGOOSE_PATH);
 }
 main().catch(err => console.log(err.message));
 
